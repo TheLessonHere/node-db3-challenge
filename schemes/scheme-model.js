443d72cebfaa372f04abe1185ac5ae29b7b369f6
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-09-11 13:54:59 -0600

    Scheme-model updated with helpers. Added db-config file

---
+++ schemes/scheme-model.js
@@ -0,0 +1,47 @@
+const db = require('../data/db-config');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    insert,
+    update,
+    remove
+};
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes')
+    .where({ id })
+    .first();
+}
+
+function findSteps(scheme_id) {
+    return db('steps as st')
+    .join('schemes as sc', 'sc.id', 'st.scheme_id')
+    .select('st.id', 'st.step_number', 'st.instructions', 'sc.scheme_name as schemeName')
+    .where('st.scheme_id', scheme_id)
+}
+
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme)
+    .then(id => {
+        return findById(id[0]);
+    });
+}
+
+function update(changes, id) {
+    return db('schemes')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+    return db('schemes')
+      .where('id', id)
+      .del();
+  }
